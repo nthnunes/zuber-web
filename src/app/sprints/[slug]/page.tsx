9b506a1e177b19ac31f1bfa15244914be7493a6b
Nathan Nunes <nathanpedronunes@hotmail.com>
2024-05-05 20:32:58 -0300

    fix: adjust back button position

--- src/app/sprints/[slug]/page.tsx
+++ src/app/sprints/[slug]/page.tsx
@@ -24,43 +24,41 @@ export default async function Sprints({ params }: SprintsProps) {
   const sprints = await getData(params.slug as string);
   return (
     <>
-      <div className="absolute top-2.5 left-2.5 pt-[6px]">
-        <Button className="">
-          <Link
-            href={`/device/${params.slug}`}
-            className="flex items-center justify-center"
-          >
-            <ChevronLeft />
-            Voltar
-          </Link>
-        </Button>
-        <section className="container h-40 mt-20">
-          <h1 className="text-3xl text-white">Histórico de Corridas</h1>
-          <div className="flex flex-col gap-4 w-full sm:w1/2 sm:flex-row mt-4">
-            {sprints != "" ? (
-              sprints.map((item: any) => (
-                <Card key={item.id}>
-                  <CardHeader>
-                    <CardTitle>Corrida Id : {item.id}</CardTitle>
-                    <CardDescription>
-                      Clique para ver mais Informações
-                    </CardDescription>
-                    <CardContent className="p-0">
-                      <p>Dispositivo Id {item.deviceId}</p>
-                      <p>
-                        Iniciado em{" "}
-                        {format(new Date(item.date_start), "yyyy-MM-dd HH")}
-                      </p>
-                    </CardContent>
-                  </CardHeader>
-                </Card>
-              ))
-            ) : (
-              <h1>Sem Corridas</h1>
-            )}
-          </div>
-        </section>
-      </div>
+      <Button className="absolute top-2.5 left-20 mt-[6px]">
+        <Link
+          href={`/device/${params.slug}`}
+          className="flex items-center justify-center"
+        >
+          <ChevronLeft />
+          Voltar
+        </Link>
+      </Button>
+      <section className="container h-40 mt-20">
+        <h1 className="text-3xl text-white">Histórico de Corridas</h1>
+        <div className="flex flex-col gap-4 w-full sm:w1/2 sm:flex-row mt-4">
+          {sprints != "" ? (
+            sprints.map((item: any) => (
+              <Card key={item.id}>
+                <CardHeader>
+                  <CardTitle>Corrida Id : {item.id}</CardTitle>
+                  <CardDescription>
+                    Clique para ver mais Informações
+                  </CardDescription>
+                  <CardContent className="p-0">
+                    <p>Dispositivo Id {item.deviceId}</p>
+                    <p>
+                      Iniciado em{" "}
+                      {format(new Date(item.date_start), "yyyy-MM-dd HH")}
+                    </p>
+                  </CardContent>
+                </CardHeader>
+              </Card>
+            ))
+          ) : (
+            <h1>Sem Corridas</h1>
+          )}
+        </div>
+      </section>
     </>
   );
 }
