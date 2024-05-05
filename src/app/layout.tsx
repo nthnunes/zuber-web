443553aebef335f2add732e7b0feccd7e22a5269
Nathan Nunes <nathanpedronunes@hotmail.com>
2024-05-05 14:53:40 -0300

    fix: adjust map to cover entire screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>
+      <body className={`${inter.className} overflow-hidden`}>
         <ThemeProvider
         attribute="class"
         defaultTheme="system"
--- src/app/live-sprints/[slug]/page.tsx
+++ src/app/live-sprints/[slug]/page.tsx
@@ -1,60 +1,62 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { ChevronLeft } from 'lucide-react';
-import React, { useEffect, useState } from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { ChevronLeft } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 interface LiveSprintsProps {
-    params  : {
-        slug ?: string
-    }
+  params: {
+    slug?: string;
+  };
 }
 
-export default function LiveSprints ({params} : LiveSprintsProps)  {
-   
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: "AIzaSyBo3h39Vto7jTzZCPsosGIJLGA5QWsSOX4"
-      })
+export default function LiveSprints({ params }: LiveSprintsProps) {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBo3h39Vto7jTzZCPsosGIJLGA5QWsSOX4",
+  });
 
-     
-      const [currentLocation,setCurrentLocation] = useState<any>([])
+  const [currentLocation, setCurrentLocation] = useState<any>([]);
 
-      const position = {
-        lat : currentLocation.lat,
-        lng : currentLocation.lng,
-      }
+  const position = {
+    lat: currentLocation.lat,
+    lng: currentLocation.lng,
+  };
 
-      useEffect(() => {
-       const Getposition  = (position : any)  => {
-        const dados : any = {
-            lat: position.coords.latitude, lng: position.coords.longitude
-        }
-        setCurrentLocation(dados)  
-       }
-        navigator.geolocation.getCurrentPosition(Getposition)
-      },[]);
-      
+  useEffect(() => {
+    const Getposition = (position: any) => {
+      const dados: any = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      setCurrentLocation(dados);
+    };
+    navigator.geolocation.getCurrentPosition(Getposition);
+  }, []);
 
-    return ( 
-        <>
-            <Button className="m-4"><Link href={`/device/${params.slug}`} className="flex items-center justify-center"><ChevronLeft />Voltar</Link></Button>
-            <section className="container h-40 mt-20">
-            <h1 className="text-3xl text-white">Corridas ao vivo</h1>
-            <div className="w-1/2 h-[20rem] rounded-sm">
-             {isLoaded && (
-                 <GoogleMap
-                 mapContainerStyle={{width : "100%",height : "100%",borderRadius : "10px"}}
-                 center={position}
-                 zoom={15}
-               >
-                <Marker position={position}/>
-               </GoogleMap>
-             )}
-            </div>
-            </section>
-        </>
-     );
+  return (
+    <>
+      <div className="absolute top-2.5 left-2.5 z-50 pl-16 pt-[6px]">
+        <Button className="p-2">
+          <Link
+            href={`/device/${params.slug}`}
+            className="flex items-center justify-center"
+          >
+            <ChevronLeft size={24} />
+            Voltar
+          </Link>
+        </Button>
+      </div>
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={{ width: "100vw", height: "90vh" }}
+          center={currentLocation}
+          zoom={15}
+        >
+          <Marker position={currentLocation} />
+        </GoogleMap>
+      )}
+    </>
+  );
 }
- 
