913ca7b0c399a1c4300b75b8dfd119abbcf96e6b
Nathan Nunes <nathanpedronunes@hotmail.com>
2024-05-07 16:40:28 -0300

    feat: connect current location with websocket

--- src/app/live-sprints/[slug]/page.tsx
+++ src/app/live-sprints/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import io, { Socket } from "socket.io-client";
 
 interface LiveSprintsProps {
   params: {
@@ -17,37 +18,52 @@ export default function LiveSprints({ params }: LiveSprintsProps) {
     googleMapsApiKey: "AIzaSyBo3h39Vto7jTzZCPsosGIJLGA5QWsSOX4",
   });
 
-  const [currentLocation, setCurrentLocation] = useState<any>([]);
-
-  const position = {
-    lat: currentLocation.lat,
-    lng: currentLocation.lng,
-  };
+  const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 });
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    const Getposition = (position: any) => {
-      const dados: any = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      };
-      setCurrentLocation(dados);
+    const socketUrl = "ws://localhost:3001";
+    const newSocket = io(socketUrl);
+
+    setSocket(newSocket);
+
+    newSocket.on("connect", () => {
+      console.log("Conectado ao servidor WebSocket");
+      newSocket.emit(
+        "getSprintGeoLocations",
+        "9c973427-7171-4481-93e0-7f8fb388ba17"
+      );
+    });
+
+    newSocket.on("sprintGeoLocations", (data: any) => {
+      setCurrentLocation({
+        lat: parseFloat(data.lat),
+        lng: parseFloat(data.lon),
+      });
+    });
+
+    return () => {
+      newSocket.off("connect");
+      newSocket.off("sprintGeoLocations");
+      newSocket.close();
     };
-    navigator.geolocation.getCurrentPosition(Getposition);
   }, []);
+
+  useEffect(() => {
+    console.log(currentLocation);
+  }, [currentLocation]);
 
   return (
     <>
-      <div className="absolute top-2.5 left-2.5 z-50 pl-16 pt-[6px]">
-        <Button className="p-2">
-          <Link
-            href={`/device/${params.slug}`}
-            className="flex items-center justify-center"
-          >
-            <ChevronLeft size={24} />
-            Voltar
-          </Link>
-        </Button>
-      </div>
+      <Button className="absolute top-2.5 left-20 mt-[6px]">
+        <Link
+          href={`/device/${params.slug}`}
+          className="flex items-center justify-center"
+        >
+          <ChevronLeft size={24} />
+          Voltar
+        </Link>
+      </Button>
       {isLoaded && (
         <GoogleMap
           mapContainerStyle={{ width: "100vw", height: "90vh" }}
